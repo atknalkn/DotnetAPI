1c2fb965799392accb605e12d11601dabd8b81f0
Atakan Alkan <atakanalkan305@gmail.com>
2024-08-18 17:08:25 +0300

    some extentions

--- Program.cs
+++ Program.cs
@@ -1,32 +1,34 @@
-using System.ComponentModel.Design;
-using Microsoft.AspNetCore.Cors.Infrastructure;
+using DotnetAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 builder.Services.AddControllers();
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors((options) =>
-{
-    options.AddPolicy("DevCors", (corsbuilder) =>
     {
-            corsbuilder.WithOrigins("http://localhost4200","http://localhost3000","http://localhost8000")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials();
-    });
-     options.AddPolicy("ProdCors", (corsbuilder) =>
-    {
-            corsbuilder.WithOrigins("https://myProductionSite.com")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials();
+        options.AddPolicy("DevCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+        options.AddPolicy("ProdCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("https://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
     });
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,13 +43,9 @@ else
     app.UseCors("ProdCors");
     app.UseHttpsRedirection();
 }
-app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.MapControllers();
-// app.MapGet("/weatherforecast", () =>
-// {
 
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
 app.Run();
-
